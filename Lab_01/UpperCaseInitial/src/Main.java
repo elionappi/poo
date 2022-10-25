7d31e18266a9e9b7fba54ab1d000c605bb1fc24d
Eliodoro Nappi <42388003+elionappi@users.noreply.github.com>
2022-10-25 20:11:17 +0200

    lab 5

--- Lab_01/UpperCaseInitial/src/Main.java
+++ Lab_01/UpperCaseInitial/src/Main.java
@@ -3,7 +3,15 @@ public class Main {
         String[] words = {"", "a", "alice", "bob", "pippo", "pluto"};
 
         for (String word : words) {
-            /* TODO */
+            if (!word.isEmpty()) {
+                String initial = String.valueOf(word.charAt(0));
+                String upperCaseInitial = initial.toUpperCase();
+                String remainingLetters = word.substring(1);
+                String upperCaseWord = upperCaseInitial + remainingLetters;
+                System.out.println(upperCaseWord);
+            } else {
+                System.out.println();
+            }
         }
     }
 }
